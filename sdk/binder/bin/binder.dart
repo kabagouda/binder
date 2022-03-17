df47f72df10984a52478854ab12d70b067487853
kabagouda <technodor7@gmail.com>
2022-03-17 13:09:38 +0000

    update binder.dart

--- sdk/binder/bin/binder.dart
+++ sdk/binder/bin/binder.dart
@@ -8,25 +8,41 @@ void main(List<String> arguments) async {
   //  Arguments here will be passed from the command line .
   String currentDirectory = Directory.current.path;
   // If no arguments are passed, show the help message.
+  // 1-
+
   if (arguments.isEmpty) {
     print(helpMessage()); //to print help message in console .
     exit(0);
   }
+
+  //2-
+
   // If the first argument is create, create a new project.
   else if (arguments[0] == 'create') {
-    String projectName =
-        arguments[1]; // get the project name from the command line.
-    String projectPath = join(currentDirectory,
-        projectName); // get the project path by joining the current directory and the project name.
-    //  Execute the create function. It will create a new project.(create.dart)
-    executeCreate(projectName, currentDirectory, projectPath);
+    if (arguments[1].isEmpty) {
+      print('Please provide the project name.');
+      exit(0);
+    } else {
+      String projectName =
+          arguments[1]; // get the project name from the command line.
+      String projectPath = join(currentDirectory,
+          projectName); // get the project path by joining the current directory and the project name.
+      //  Execute the create function. It will create a new project.(create.dart)
+      executeCreate(projectName, currentDirectory, projectPath);
+    }
   }
+
+  //3-
+
   // If the first argument is run, run the project.
   else if (arguments[0] == 'run') {
     //  Execute the run function. It will run the project.(run.dart)
     executeRun();
     //TODO : Ideal should be to verify if it's binder project or not.
   }
+
+  //4-
+  
   // If the first argument is build, build the project.
   else if (arguments[0] == 'build') {
     //  Execute the build function. It will build the project.(build.dart)
@@ -35,9 +51,9 @@ void main(List<String> arguments) async {
     } else if (arguments[1] == '--debug' || arguments[1] == '-d') {
       executeBuildForDebug();
     } else {
-      executeBuildForDebug();
+      print('Invalid build argument');
+      print(helpMessage());
     }
-    // executeBuild(arguments[1]); // arguments[1] is example --release or --debug
   }
 
   //------------------------------Other unknown command
@@ -57,16 +73,16 @@ Commands:
     Creates a new project with the given name.
   run
     Runs the web project in the browser.  (run --web is the default command.) 
-  run --web  or run -w
+  run web
     Runs the web project in the browser and automatically rebuilds it when
     necessary.  
-  run --server or run -s
+  run server 
     Runs the server project .
   build 
     Builds the web project. (build --web is the default command.)
-  build --release  or build -r
+  build --release
     Builds the web project in release mode.  
-  build --debug  or build -d
+  build --debug
     Builds the web project in debug mode.    
   help  or help -h
     Shows this message.
